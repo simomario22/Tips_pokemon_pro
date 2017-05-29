175a86597debc9bba6dd8578dac09d00299a521a
simo mariok <simomario22@gmail.com>
2017-05-29 14:20:18 +0000

    banner

--- app/src/main/java/com/tips/tips_pokemon_pro/tips_pokemon_pro5.java
+++ app/src/main/java/com/tips/tips_pokemon_pro/tips_pokemon_pro5.java
@@ -3,11 +3,21 @@ package com.tips.tips_pokemon_pro;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class tips_pokemon_pro5 extends AppCompatActivity {
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 
+public class tips_pokemon_pro5 extends AppCompatActivity {
+    private AdView mAdView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips_pokemon_pro5);
+
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-3940256099942544~3347511713");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 }
