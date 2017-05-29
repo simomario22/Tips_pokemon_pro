7ed0517fcf8b56b58a5cfa637879a81f6e9e14f0
simo mariok <simomario22@gmail.com>
2017-05-29 03:35:14 +0000

    admob

--- app/src/main/java/com/tips/tips_pokemon_pro/tips_pokemon_pro.java
+++ app/src/main/java/com/tips/tips_pokemon_pro/tips_pokemon_pro.java
@@ -5,12 +5,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+
 public class tips_pokemon_pro extends AppCompatActivity {
+
+    private AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tips_pokemon_pro);
+
+        MobileAds.initialize(getApplicationContext(),
+                "ca-app-pub-3940256099942544~3347511713");
+        mAdView = (AdView) findViewById(R.id.adView);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
     }
 
     public void but5(View view) {
